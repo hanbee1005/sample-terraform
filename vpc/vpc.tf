96ae3c0351ea66fc51185611665cffa037d99b3e
hanbee <sonhanbi1002@gmail.com>
2022-01-03 17:09:07 +0900

    라우팅 테이블 생성 (인터넷게이트웨이 - 라우팅 테이블 - 퍼블릭 서브넷 연결)

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -85,3 +85,33 @@ resource "aws_internet_gateway" "this" {
     Group = local.group
   }
 }
+
+### PUBLIC ROUTING TABLE ###
+resource "aws_default_route_table" "public" {
+  default_route_table_id = aws_vpc.this.main_route_table_id
+
+  tags = {
+    Name = "${local.svc_nm}-public",
+    Creator= local.creator,
+    Group = local.group
+  }
+}
+
+# 라우팅 테이블과 인터넷 게이트웨이 연결
+resource "aws_route" "internet_gateway" {
+  count                  = length(local.public_subnets)
+  route_table_id         = aws_default_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.this.id
+
+  timeouts {
+    create = "5m"
+  }
+}
+
+# 서브넷과 라우팅 테이블 연결
+resource "aws_route_table_association" "public" {
+  count          = length(local.public_subnets)
+  subnet_id      = element(aws_subnet.public.*.id, count.index)
+  route_table_id = aws_default_route_table.public.id
+}
